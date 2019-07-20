2d64a736285225a088eb3d0679ab3f216a4f8391
ymt <mengtao.yan@hand-china.com>
2019-07-20 15:55:30 +0800

    [INIT] 微服务学习

---
+++ yanmengtao/eureka-server/src/main/java/com/sandboat/eurekaserver/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.sandboat.eurekaserver.controller;
+
+import org.jboss.logging.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        Integer r = a + b;
+        logger.info("hello 0002");
+        return r;
+    }
+}
