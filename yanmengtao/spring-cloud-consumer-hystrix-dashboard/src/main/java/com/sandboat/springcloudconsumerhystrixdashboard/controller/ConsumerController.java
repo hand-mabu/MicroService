d6e9f6f2155c1cabeaa68f70e6f57759e802cf7e
ymt <mengtao.yan@hand-china.com>
2019-07-21 22:35:16 +0800

    [ADD] 添加 zipkin

---
+++ yanmengtao/spring-cloud-consumer-hystrix-dashboard/src/main/java/com/sandboat/springcloudconsumerhystrixdashboard/controller/ConsumerController.java
@@ -0,0 +1,19 @@
+package com.sandboat.springcloudconsumerhystrixdashboard.controller;
+
+import com.sandboat.springcloudconsumerhystrixdashboard.remote.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    HelloRemote HelloRemote;
+
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return HelloRemote.hello(name);
+    }
+}
