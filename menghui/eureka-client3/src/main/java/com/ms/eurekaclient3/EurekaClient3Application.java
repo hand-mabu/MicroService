71b319ea50d4418bf578882377848426d3f4b717
刘梦辉 <menghui.liu@hand-china.com>
2019-07-24 20:02:11 +0800

    [FIX] 添加端口

--- menghui/eureka-client3/src/main/java/com/ms/eurekaclient3/EurekaClient3Application.java
+++ menghui/eureka-client3/src/main/java/com/ms/eurekaclient3/EurekaClient3Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +16,12 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 public class EurekaClient3Application {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClient3Application.class, args);
--- menghui/service/src/main/java/com/ms/service/controller/UserController.java
+++ menghui/service/src/main/java/com/ms/service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ms.service.controller;
 
 import com.ms.service.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,11 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("getUser/{id}")
-    public String getUser(@PathVariable int id){
-        return "服务端口8081被调用："+userService.getUser(id).toString();
+    public String getUser(@PathVariable int id) {
+        return "服务端口" + port + "被调用：" + userService.getUser(id).toString();
     }
 }
